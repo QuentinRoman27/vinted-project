3e2921f85fafe5fbfcc7916e8f0c241038fdd4ee
Alix et Quentin <alixetquentin@alixetquentin.lan>
2022-10-21 18:15:46 +0200

    clear

--- index.js
+++ index.js
@@ -10,17 +10,21 @@ app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI);
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-  secure: true,
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+//   secure: true,
+// });
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome Vinted API" });
 });
 
-const userRoutes = require("./routes/user");
-const offerRoutes = require("./routes/offer");
-app.use(userRoutes);
-app.use(offerRoutes);
+// const userRoutes = require("./routes/user");
+// const offerRoutes = require("./routes/offer");
+// app.use(userRoutes);
+// app.use(offerRoutes);
 
 app.all("*", (req, res) => {
   res.status(404).json({ message: "This route does not exist" });
