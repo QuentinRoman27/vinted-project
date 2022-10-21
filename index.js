e8b39acaab8315390c2d0b2cbb37fced763df22d
Alix et Quentin <alixetquentin@alixetquentin.lan>
2022-10-21 18:10:29 +0200

    deploy

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+require(`dotenv`).config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cloudinary = require("cloudinary").v2;
-require(`dotenv`).config();
-const app = express();
-app.use(express.json());
 const cors = require("cors");
+
+const app = express();
 app.use(cors());
+app.use(express.json());
+
 mongoose.connect(process.env.MONGODB_URI);
 
 cloudinary.config({
